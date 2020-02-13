98c7020f35f86f5449f53701550f0cbfa79c6326
SirBizkit <popanda.pawel@gmail.com>
2020-02-13 15:30:59 +0000

    Added VcomAI

---
+++ @VcomAI/mod.cpp
@@ -0,0 +1,23 @@
+name = "VCOMAI";
+picture = "\VcomAI\VcomAILLogo.paa";
+actionName = "Website";
+action = "https://forums.bohemia.net/forums/topic/216611-release-vcom-ai-3-dangerous-ai/";
+description = "Enhancing your ArmA 3 experience to the next level.";
+logo = "\VcomAI\VcomAILogo.paa";
+logoOver = "\VcomAI\VcomAILLogo.paa";
+tooltip = "Vcom";
+tooltipOwned = "Genesis92x";
+overview = "Bringing the AI back into the fight.";
+author = "Genesis92x";
+overviewPicture = "";
+overviewText = "VcomAI";
+overviewFootnote = "Vcom is under ARMA PUBLIC LICENSE SHARE ALIKE (APL-SA)";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = 
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
